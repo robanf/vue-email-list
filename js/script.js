16aa8faf2b64cfd604f7e48af75e47fb7b1ef7fa
Roberto <robertoanfuso98@gmail.com>
2021-07-02 14:56:45 +0200

    mi ero scordato i console log

--- js/script.js
+++ js/script.js
@@ -9,9 +9,7 @@ const app= new Vue({
             axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(element =>{
-                    console.log(element.data.response);
                     this.array.push(element.data.response)
-                    console.log(this.array);
                 })
         }
     }
